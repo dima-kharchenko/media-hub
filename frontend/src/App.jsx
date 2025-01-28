b2964c0f39e5b29d2a65e5ca38d4d6c00173fac5
Kyrylo <moisey67kirill@gmail.com>
2025-01-28 23:44:10 +0100

    Pages directory update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import Navbar from './Components/Navbar'
+import Navbar from './Components/Navbar' 
+
+
 
 function App() {
   return (
     <>
-      <Navbar />
+    <Navbar></Navbar>
     </>
   )
 }
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,14 +1,19 @@
+
+
 function Navbar(){
     return(
         <>
         <nav className="bg-surface-a10 border-b-1 border-primary-a0">
             <div className="px-5 flex flex-wrap items-center justify-between mx-auto h-12">
-                <a href="#" className="flex">
+                
+                <a href="#">
+                    
                     <i className="fa-brands fa-github text-3xl text-white"></i>
-                </a> 
+                </a>
+                 
                 <form className="flex items-center max-w-sm w-80">   
                     <div className="relative w-full">
-                        <input type="text" id="simple-search" style={{fontFamily:"Arial, FontAwesome"}} className="bg-surface-a20 h-8 text-white placeholder-neutral-400 rounded-full block w-full p-3  shadow-[0_0_1px_1px] shadow-primary-a0 hover:shadow-[0_0_5px_1px] focus:shadow-[0_0_5px_1px] duration-150 focus:outline-none" placeholder=" &#xF002; Search" />
+                        <input type="text" id="simple-search" style={{fontFamily:"Arial, FontAwesome"}} className="bg-surface-a20 h-8 text-white placeholder-neutral-400 rounded-full block w-full p-3  shadow-[0_0_1px_1px] shadow-primary-a0 hover:shadow-[0_0_5px_1px] focus:shadow-[0_0_5px_1px] duration-150 focus:outline-none selection:bg-surface-a50" placeholder=" &#xF002; Search" />
                     </div>
                 </form>
                 <i className="fa-regular fa-user text-2xl text-white"></i>

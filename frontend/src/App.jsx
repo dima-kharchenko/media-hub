5ba1146ae296a52544076d6cdae8e846dd0cda20
Kyrylo <moisey67kirill@gmail.com>
2025-01-29 00:27:54 +0100

    Added base routing, comments for better understanding, layouts, pages. Hyperlinks added to Navbar instead of 'href' elems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
-import Navbar from './Components/Navbar' 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/Abouts';
+import DefaultLayout from './Layouts/DefaultLayout';
+import AuthLayout from './Layouts/AuthLayout';
+import NotFound from './Pages/NotFound';
 
 
 
 function App() {
   return (
     <>
-    <Navbar></Navbar>
+    <Router>
+      <Routes>
+        {/* HOME PAGE WITH DEFAULT LAYOUT PERMS */}
+        <Route path="/" element={<DefaultLayout><Home /></DefaultLayout>} />
+        
+        {/* ABOUT PAGE WITH DEFAULT LAYOUT PERMS */}
+        <Route path="/about" element={<DefaultLayout><About /></DefaultLayout>} />
+
+        {/* 404 PAGE WITHOUT LAYOUT PERMS */}
+        <Route path="*" element={<NotFound />} /> 
+      </Routes>
+    </Router>
     </>
   )
 }
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 
 function Navbar(){
     return(
@@ -6,17 +6,18 @@ function Navbar(){
         <nav className="bg-surface-a10 border-b-1 border-primary-a0">
             <div className="px-5 flex flex-wrap items-center justify-between mx-auto h-12">
                 
-                <a href="#">
+                <Link to="/"> {/* logo link */}
                     
                     <i className="fa-brands fa-github text-3xl text-white"></i>
-                </a>
+                </Link>
                  
                 <form className="flex items-center max-w-sm w-80">   
                     <div className="relative w-full">
                         <input type="text" id="simple-search" style={{fontFamily:"Arial, FontAwesome"}} className="bg-surface-a20 h-8 text-white placeholder-neutral-400 rounded-full block w-full p-3  shadow-[0_0_1px_1px] shadow-primary-a0 hover:shadow-[0_0_5px_1px] focus:shadow-[0_0_5px_1px] duration-150 focus:outline-none selection:bg-surface-a50" placeholder=" &#xF002; Search" />
                     </div>
                 </form>
-                <i className="fa-regular fa-user text-2xl text-white"></i>
+                <Link to="/about"> {/* profile link */}
+                <i className="fa-regular fa-user text-2xl text-white"></i></Link>
             </div>
         </nav>
     </>

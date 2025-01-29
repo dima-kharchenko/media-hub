75fdff67b81093c57c2e2522b807522743558cda
dima-kharchenko <dmitriy020606@gmail.com>
2025-01-29 02:03:36 +0100

    Fixed merge conflict in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,19 @@
 import './App.css'
-<<<<<<< HEAD
-import Navbar from './Components/Navbar'
 import Login from './Pages/Login'
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/Abouts';
 import DefaultLayout from './Layouts/DefaultLayout';
-import AuthLayout from './Layouts/AuthLayout';
 import NotFound from './Pages/NotFound';
 
 
->>>>>>> 5ba1146ae296a52544076d6cdae8e846dd0cda20
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <div className="bg-surface-a0">
       <Login />
       </div>
-=======
     <Router>
       <Routes>
         {/* HOME PAGE WITH DEFAULT LAYOUT PERMS */}
@@ -33,7 +26,6 @@ function App() {
         <Route path="*" element={<NotFound />} /> 
       </Routes>
     </Router>
->>>>>>> 5ba1146ae296a52544076d6cdae8e846dd0cda20
     </>
   )
 }

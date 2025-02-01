5da5a935783f8d4cb831fec9966d009adcccb9c6
dima-kharchenko <dmitriy020606@gmail.com>
2025-02-01 20:32:31 +0100

    Deleted frontend copies

--- frontend/frontend/frontend/src/Components/Input.jsx
+++
@@ -1,7 +0,0 @@
-function Input(props){
-    return(
-        <input type={props.type} name={props.name} id={props.id} className={`w-full h-11 px-6 rounded-md bg-surface-a20 text-sm text-white placeholder-white shadow-[0_0_1px_1px] shadow-primary-a10 hover:shadow-[0_0_4px_1px] focus:shadow-[0_0_4px_1px] duration-150 focus:outline-none ${props.className || ""}`} placeholder={props.placeholder} required=""/>
-    );
-}
-
-export default Input
--- frontend/frontend/frontend/src/Pages/About.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-function About() {
-  return <h1>About Us Page</h1>;
-}
-
-export default About;
